a295d4aaf0dfe68e148475653672e2469642db76
Clavvv <Schpeen@proton.me>
2023-01-28 15:21:07 -0800

    amendment to data intake processes

--- Scripts/format_data.py
+++ Scripts/format_data.py
@@ -9,15 +9,18 @@ import numpy as np
 def parse(json_arr):
     nan= np.nan
     rows= []
+    bills= []
 
     for j in json_arr:
+        bills.append(j['bill'])
+        del j['bill']
         rows.append(j)
 
-    df= pd.DataFrame.from_dict(rows, converters={'bill':parse()}, orient='columns')
 
-
-
-
+    df= pd.DataFrame.from_dict(rows, orient='columns')
+    df2=pd.DataFrame.from_dict(bills, orient='columns')
+    final= pd.merge(df, df2, left_index=True, right_index=True)
+    final.drop(['republican', 'independent', 'democratic'], axis=1, inplace=True)
 
     return
 
@@ -36,6 +39,6 @@ def parse(json_arr):
 
 
 if __name__ == "__main__":
-    r= handle_api(Daily= True)
+    r= handle_api(byMonth= True, y='2023', m='01')
     parse(r)
 
