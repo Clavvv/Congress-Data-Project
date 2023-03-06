3a5958b815ff853550ac712dab3a9d5c2fcca2fd
Ryan <93360459+Clavvv@users.noreply.github.com>
2023-03-06 14:57:42 -0800

    Update addpktobasetables.sql

--- Scripts/addpktobasetables.sql
+++ Scripts/addpktobasetables.sql
@@ -7,13 +7,13 @@ BEGIN
     LEFT JOIN information_schema.table_constraints tco 
           ON tab.table_schema = tco.table_schema
           AND tab.table_name = tco.table_name 
-          AND tco.constraint_type = 'p_id'
+          AND tco.constraint_type = 'PRIMARY KEY'
     WHERE tab.table_type = 'BASE TABLE'
           AND tab.table_schema not in ('pg_catalog', 'information_schema')
           AND tco.constraint_name is null
     ORDER BY table_schema, table_name 
     LOOP 
-        EXECUTE 'ALTER TABLE "' || row.table_schema || '"."' || row.table_name  || '" ADD COLUMN PRIMARY_KEY SERIAL PRIMARY KEY';
+        EXECUTE 'ALTER TABLE "' || row.table_schema || '"."' || row.table_name  || '" ADD COLUMN p_id SERIAL PRIMARY KEY';
     END LOOP;  
 END;
 $$;
