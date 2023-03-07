cfd7acab149fb02f202f61a0d3c72ae42cff3bc4
Clavvv <Schpeen@proton.me>
2023-03-07 13:57:47 -0800

    working spatial overlap func

--- Scripts/make_overlap_percentages.sql
+++ Scripts/make_overlap_percentages.sql
@@ -1,5 +1,14 @@
-SELECT county.*, cd.primary_key
-FROM test_county county, test_cd cd
-WHERE ST_Intersects(ST_Transform(county.geometry, 102003), cd.geometry)
-  AND (ST_Area(ST_Transform(ST_Intersection(county.geometry, cd.geometry), 102003)) /
-       ST_Area(ST_Transform(county.geometry, 102003))) > 0.01;
+create table test_output as
+	SELECT county.*, cd.p_id as parent_id,
+       	(ST_Area(ST_Transform(ST_Intersection(county.geometry, cd.geometry), 102003)) /
+        	ST_Area(ST_Transform(county.geometry, 102003))) * 100 as overlap_pct
+	FROM test_county county
+	INNER JOIN test_congress cd
+	ON ST_Intersects(ST_Transform(county.geometry, 102003), cd.geometry)
+  	AND (ST_Area(ST_Transform(ST_Intersection(county.geometry, cd.geometry), 102003)) /
+       	ST_Area(ST_Transform(county.geometry, 102003))) > 0.01;SELECT county.*, cd.p_id as parent_id
+  FROM test_county county
+  INNER JOIN test_congress cd
+  ON ST_Intersects(ST_Transform(county.geometry, 102003), cd.geometry)
+    AND (ST_Area(ST_Transform(ST_Intersection(county.geometry, cd.geometry), 102003)) /
+        ST_Area(ST_Transform(county.geometry, 102003))) > 0.01;
