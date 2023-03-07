db20da446865eb95af6cfae6ec184a4efab516bd
Clavvv <Schpeen@proton.me>
2023-03-07 14:46:40 -0800

    fix query bug

--- Scripts/make_overlap_percentages.sql
+++ Scripts/make_overlap_percentages.sql
@@ -6,9 +6,4 @@ create table test_output as
 	INNER JOIN test_congress cd
 	ON ST_Intersects(ST_Transform(county.geometry, 102003), cd.geometry)
   	AND (ST_Area(ST_Transform(ST_Intersection(county.geometry, cd.geometry), 102003)) /
-       	ST_Area(ST_Transform(county.geometry, 102003))) > 0.01;SELECT county.*, cd.p_id as parent_id
-  FROM test_county county
-  INNER JOIN test_congress cd
-  ON ST_Intersects(ST_Transform(county.geometry, 102003), cd.geometry)
-    AND (ST_Area(ST_Transform(ST_Intersection(county.geometry, cd.geometry), 102003)) /
-        ST_Area(ST_Transform(county.geometry, 102003))) > 0.01;
+       	ST_Area(ST_Transform(county.geometry, 102003))) > 0.01;
