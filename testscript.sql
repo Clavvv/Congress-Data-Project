0a1083308e71f4044e5fda00a63fa9429bb0a586
Clavvv <Schpeen@proton.me>
2023-03-09 15:40:24 -0800

    minor changes

--- testscript.sql
+++
@@ -1,4 +0,0 @@
-select test_output.overlap_pct, "NAME", "CD116FP" from test_congress
-INNER JOIN test_output ON test_output.parent_id=test_congress.p_id
-AND test_output."STATEFP" ='06'
-ORDER BY "CD116FP";
